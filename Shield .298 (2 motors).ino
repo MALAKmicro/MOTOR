2769f02ed460e6667b8298a6ed5c610f0ea3bc7d
MALAKmicro <92943033+MALAKmicro@users.noreply.github.com>
2021-10-25 17:44:45 +0200

    Update and rename Shield 298 (2 motors).ino to Shield .298 (2 motors).ino

--- Shield .298 (2 motors).ino
+++ Shield .298 (2 motors).ino
@@ -25,21 +25,22 @@ digitalWrite(IN1, LOW);
 digitalWrite(IN2, HIGH);
 digitalWrite(IN3, LOW);
 digitalWrite(IN4, HIGH);
-    delay(5000);
+    delay(5000);// 5 seconds
 
 // stop motor 1 & 2
 digitalWrite(IN1, LOW);
 digitalWrite(IN2, LOW);
 digitalWrite(IN3, LOW);
 digitalWrite(IN4, LOW);
-  delay(5000);
+  delay(5000);// 5 seconds
 
 //another direction motor 1 & 2
 digitalWrite(IN1, HIGH);
 digitalWrite(IN2, LOW);
 digitalWrite(IN3, HIGH);
 digitalWrite(IN4, LOW);
-delay(5000);
+delay(5000);// 5 seconds
 
 }
+
 
